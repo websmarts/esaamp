176725f37b8ba48ddaa576e717b2d548c25c4b1e
websmarts <iwmaclagan@bigpond.com>
2018-01-30 16:56:59 +1100

    mockup tweaks inc logo with checkbox

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,13 +22,13 @@
   <body>
 <div id="app">
 
-         <section class="hero is-info is-medium is-bold">
+    <section class="hero is-info is-medium is-bold">
     <div class="hero-head">
       <nav class="navbar">
         <div class="container">
           <div class="navbar-brand">
             <a class="navbar-item" href="../">
-              <img src="images/logo-white.png" alt="Logo">
+              <img src="images/logo-with-tick-white.png" alt="Logo">
             </a>
             <span class="navbar-burger burger" data-target="navbarMenu">
               <span></span>
@@ -78,121 +78,19 @@
 
   <div class="box cta">
     <p class="has-text-centered">
-      <span class="tag is-primary">New</span> Now with support for wingdings as well as slingdings.
+      <span class="tag is-danger">New</span> Now with support for wingdings as well as slingdings.
     </p>
   </div>
 
-  <section class="container">
-    <div class="columns features">
-      <div class="column is-4">
-        <div class="card">
-          <div class="card-image has-text-centered">
-                <i class="fa fa-database"></i>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <h4>DATA CENTRALISATION </h4>
-              <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam. Purus semper eget duis.</p>
-              <p><a href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column is-4">
-        <div class="card">
-          <div class="card-image has-text-centered">
-              <i class="fa fa-id-card-o"></i>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <h4>SAFTETY SOLUTION.</h4>
-              <p>Ut venenatis tellus in metus vulputate. Amet consectetur adipiscing elit pellentesque. Sed arcu non odio euismod lacinia at quis risus. Faucibus turpis in eu mi bibendum neque egestas cmonsu songue. Phasellus vestibulum lorem sed risus.</p>
-              <p><a href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column is-4">
-        <div class="card">
-          <div class="card-image has-text-centered">
-              <i class="fa fa-table"></i>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <h4> ASSET MANAGEMENT.  </h4>
-              <p>Imperdiet dui accumsan sit amet nulla facilisi morbi. Fusce ut placerat orci nulla pellentesque dignissim enim. Libero id faucibus nisl tincidunt eget nullam. Commodo viverra maecenas accumsan lacus vel facilisis.</p>
-              <p><a href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="intro column is-8 is-offset-2 has-text-centered">
-      <h2 class="title">eSAAMP Basics</h2><br>
-      <p class="subtitle">Vel fringilla est ullamcorper eget nulla facilisi. Nulla facilisi nullam vehicula ipsum a. Neque egestas congue quisque egestas diam in arcu cursus.</p>
-    </div>
-
-  </section>
-
-  <section class="container">
-    <div class="columns features">
-      <div class="column is-3">
-        <div class="card">
-
-          <div class="card-content">
-            <div class="content">
-              <h4>What is eSAAMP? </h4>
-              <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam. Purus semper eget duis.</p>
-              <p><a href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column is-3">
-        <div class="card">
-
-          <div class="card-content">
-            <div class="content">
-              <h4>Why eSAAMP?</h4>
-              <p>Ut venenatis tellus in metus vulputate. Amet consectetur adipiscing elit pellentesque. Sed arcu non odio euismod lacinia at quis risus. Faucibus turpis in eu mi bibendum neque egestas cmonsu songue. Phasellus vestibulum lorem sed risus.</p>
-              <p><a href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column is-3">
-        <div class="card">
-
-          <div class="card-content">
-            <div class="content">
-              <h4> eSAAMP benefits  </h4>
-              <p>Imperdiet dui accumsan sit amet nulla facilisi morbi. Fusce ut placerat orci nulla pellentesque dignissim enim. Libero id faucibus nisl tincidunt eget nullam. Commodo viverra maecenas accumsan lacus vel facilisis. mperdiet dui accumsan sit amet nulla facilisi.</p>
-              <p><a href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="column is-3">
-        <div class="card">
-
-          <div class="card-content">
-            <div class="content">
-              <h4> How do I get started?  </h4>
-              <p>Imperdiet dui accumsan sit amet nulla facilisi morbi. Fusce ut placerat orci nulla pellentesque dignissim enim. Libero id faucibus nisl tincidunt eget nullam. Commodo viverra maecenas accumsan lacus vel facilisis. mperdiet dui accumsan sit amet nulla facilisi.</p>
-              <p><a href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+<section class="container">
+    @yield('content')
 </section>
+
   <footer class="footer">
     <div class="container">
       <div class="content has-text-centered">
         <p>
-          <strong>eSAAMP</strong> by <a href="https://github.com/dansup">Warragul Line Service</a>.
+          <strong>eSAAMP</strong> by <a href="#">Warragul Linen Service</a>.
         </p>
 
 
@@ -200,6 +98,17 @@
     </div>
   </footer>
   </div>
+  <!-- Bulm menu support -->
+ <script>
+    (function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
+</script>
     @yield('scripts')
   </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,112 @@
 @extends('layout')
 
 @section('content')
-<h1>Welcome to eSaamp Home</h1>
-<div id="app">
-    <example-component></example-component>
-</div>
+
+    <div class="columns features">
+      <div class="column is-4">
+        <div class="card">
+          <div class="card-image has-text-centered">
+                <i class="fa fa-database"></i>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <h4>DATA CENTRALISATION </h4>
+              <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam. Purus semper eget duis.</p>
+              <p><a href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column is-4">
+        <div class="card">
+          <div class="card-image has-text-centered">
+              <i class="fa fa-id-card-o"></i>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <h4>SAFTETY SOLUTION.</h4>
+              <p>Ut venenatis tellus in metus vulputate. Amet consectetur adipiscing elit pellentesque. Sed arcu non odio euismod lacinia at quis risus. Faucibus turpis in eu mi bibendum neque egestas cmonsu songue. Phasellus vestibulum lorem sed risus.</p>
+              <p><a href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column is-4">
+        <div class="card">
+          <div class="card-image has-text-centered">
+              <i class="fa fa-table"></i>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <h4> ASSET MANAGEMENT.  </h4>
+              <p>Imperdiet dui accumsan sit amet nulla facilisi morbi. Fusce ut placerat orci nulla pellentesque dignissim enim. Libero id faucibus nisl tincidunt eget nullam. Commodo viverra maecenas accumsan lacus vel facilisis.</p>
+              <p><a href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="intro column is-8 is-offset-2 has-text-centered">
+      <h2 class="title">eSAAMP Basics</h2><br>
+      <p class="subtitle">Vel fringilla est ullamcorper eget nulla facilisi. Nulla facilisi nullam vehicula ipsum a. Neque egestas congue quisque egestas diam in arcu cursus.</p>
+    </div>
+
+  </section>
+
+  <section class="container">
+    <div class="columns features">
+      <div class="column is-3">
+        <div class="card">
+
+          <div class="card-content">
+            <div class="content">
+              <h4>What is eSAAMP? </h4>
+              <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam. Purus semper eget duis.</p>
+              <p><a href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column is-3">
+        <div class="card">
+
+          <div class="card-content">
+            <div class="content">
+              <h4>Why eSAAMP?</h4>
+              <p>Ut venenatis tellus in metus vulputate. Amet consectetur adipiscing elit pellentesque. Sed arcu non odio euismod lacinia at quis risus. Faucibus turpis in eu mi bibendum neque egestas cmonsu songue. Phasellus vestibulum lorem sed risus.</p>
+              <p><a href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column is-3">
+        <div class="card">
+
+          <div class="card-content">
+            <div class="content">
+              <h4> eSAAMP benefits  </h4>
+              <p>Imperdiet dui accumsan sit amet nulla facilisi morbi. Fusce ut placerat orci nulla pellentesque dignissim enim. Libero id faucibus nisl tincidunt eget nullam. Commodo viverra maecenas accumsan lacus vel facilisis. mperdiet dui accumsan sit amet nulla facilisi.</p>
+              <p><a href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="column is-3">
+        <div class="card">
+
+          <div class="card-content">
+            <div class="content">
+              <h4> How do I get started?  </h4>
+              <p>Imperdiet dui accumsan sit amet nulla facilisi morbi. Fusce ut placerat orci nulla pellentesque dignissim enim. Libero id faucibus nisl tincidunt eget nullam. Commodo viverra maecenas accumsan lacus vel facilisis. mperdiet dui accumsan sit amet nulla facilisi.</p>
+              <p><a href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 @endsection
 
